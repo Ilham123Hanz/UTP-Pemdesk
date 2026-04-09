28a232a7963f7499832acbe715d53368a3b376d6
Ilham123Hanz <ilhamsaputrazeroone01@gmail.com>
2026-04-09 14:08:41 +0700

    Add project files.

---
+++ Main Menu/Form1.Designer.vb
@@ -0,0 +1,500 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Panel6 = New Panel()
+        Panel7 = New Panel()
+        Panel3 = New Panel()
+        Panel8 = New Panel()
+        Panel4 = New Panel()
+        Panel10 = New Panel()
+        Panel11 = New Panel()
+        Panel12 = New Panel()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        Panel2 = New Panel()
+        Panel5 = New Panel()
+        Panel9 = New Panel()
+        Panel13 = New Panel()
+        Panel14 = New Panel()
+        Panel15 = New Panel()
+        Panel16 = New Panel()
+        Panel17 = New Panel()
+        Panel18 = New Panel()
+        Panel19 = New Panel()
+        Panel20 = New Panel()
+        Panel21 = New Panel()
+        Panel22 = New Panel()
+        Panel23 = New Panel()
+        Panel24 = New Panel()
+        Panel25 = New Panel()
+        Panel26 = New Panel()
+        Panel27 = New Panel()
+        Panel28 = New Panel()
+        Panel29 = New Panel()
+        Panel30 = New Panel()
+        Panel31 = New Panel()
+        Panel32 = New Panel()
+        Panel33 = New Panel()
+        Panel34 = New Panel()
+        Panel35 = New Panel()
+        Panel36 = New Panel()
+        Panel37 = New Panel()
+        Panel38 = New Panel()
+        Panel39 = New Panel()
+        Button1 = New Button()
+        Panel1.SuspendLayout()
+        Panel7.SuspendLayout()
+        Panel3.SuspendLayout()
+        Panel4.SuspendLayout()
+        Panel10.SuspendLayout()
+        Panel11.SuspendLayout()
+        FlowLayoutPanel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel5.SuspendLayout()
+        Panel13.SuspendLayout()
+        Panel15.SuspendLayout()
+        Panel16.SuspendLayout()
+        Panel18.SuspendLayout()
+        Panel20.SuspendLayout()
+        Panel21.SuspendLayout()
+        Panel23.SuspendLayout()
+        Panel25.SuspendLayout()
+        Panel26.SuspendLayout()
+        Panel28.SuspendLayout()
+        Panel30.SuspendLayout()
+        Panel31.SuspendLayout()
+        Panel33.SuspendLayout()
+        Panel35.SuspendLayout()
+        Panel36.SuspendLayout()
+        Panel38.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Panel8)
+        Panel1.Controls.Add(Panel6)
+        Panel1.Controls.Add(Panel7)
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1199, 148)
+        Panel1.TabIndex = 0
+        ' 
+        ' Panel6
+        ' 
+        Panel6.Location = New Point(413, 147)
+        Panel6.Name = "Panel6"
+        Panel6.Size = New Size(300, 150)
+        Panel6.TabIndex = 1
+        ' 
+        ' Panel7
+        ' 
+        Panel7.Controls.Add(Panel3)
+        Panel7.Location = New Point(356, 147)
+        Panel7.Name = "Panel7"
+        Panel7.Size = New Size(300, 150)
+        Panel7.TabIndex = 1
+        ' 
+        ' Panel3
+        ' 
+        Panel3.Controls.Add(Panel4)
+        Panel3.Location = New Point(3, 0)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(359, 203)
+        Panel3.TabIndex = 3
+        ' 
+        ' Panel8
+        ' 
+        Panel8.Location = New Point(840, 147)
+        Panel8.Name = "Panel8"
+        Panel8.Size = New Size(359, 203)
+        Panel8.TabIndex = 3
+        ' 
+        ' Panel4
+        ' 
+        Panel4.Controls.Add(Panel10)
+        Panel4.Location = New Point(29, 0)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(382, 203)
+        Panel4.TabIndex = 3
+        ' 
+        ' Panel10
+        ' 
+        Panel10.Controls.Add(Panel11)
+        Panel10.Location = New Point(3, 0)
+        Panel10.Name = "Panel10"
+        Panel10.Size = New Size(382, 203)
+        Panel10.TabIndex = 5
+        ' 
+        ' Panel11
+        ' 
+        Panel11.Controls.Add(Panel12)
+        Panel11.Location = New Point(3, 0)
+        Panel11.Name = "Panel11"
+        Panel11.Size = New Size(382, 203)
+        Panel11.TabIndex = 4
+        ' 
+        ' Panel12
+        ' 
+        Panel12.Location = New Point(416, 0)
+        Panel12.Name = "Panel12"
+        Panel12.Size = New Size(420, 203)
+        Panel12.TabIndex = 3
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Controls.Add(Panel2)
+        FlowLayoutPanel1.Controls.Add(Panel15)
+        FlowLayoutPanel1.Controls.Add(Panel20)
+        FlowLayoutPanel1.Controls.Add(Panel25)
+        FlowLayoutPanel1.Controls.Add(Panel30)
+        FlowLayoutPanel1.Controls.Add(Panel35)
+        FlowLayoutPanel1.Location = New Point(0, 147)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(1199, 406)
+        FlowLayoutPanel1.TabIndex = 4
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Panel13)
+        Panel2.Controls.Add(Panel5)
+        Panel2.Location = New Point(3, 3)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(385, 197)
+        Panel2.TabIndex = 0
+        ' 
+        ' Panel5
+        ' 
+        Panel5.Controls.Add(Panel9)
+        Panel5.Location = New Point(9, 200)
+        Panel5.Name = "Panel5"
+        Panel5.Size = New Size(382, 203)
+        Panel5.TabIndex = 3
+        ' 
+        ' Panel9
+        ' 
+        Panel9.Location = New Point(416, 0)
+        Panel9.Name = "Panel9"
+        Panel9.Size = New Size(420, 203)
+        Panel9.TabIndex = 3
+        ' 
+        ' Panel13
+        ' 
+        Panel13.Controls.Add(Panel14)
+        Panel13.Location = New Point(3, 197)
+        Panel13.Name = "Panel13"
+        Panel13.Size = New Size(382, 203)
+        Panel13.TabIndex = 4
+        ' 
+        ' Panel14
+        ' 
+        Panel14.Location = New Point(416, 0)
+        Panel14.Name = "Panel14"
+        Panel14.Size = New Size(420, 203)
+        Panel14.TabIndex = 3
+        ' 
+        ' Panel15
+        ' 
+        Panel15.Controls.Add(Panel16)
+        Panel15.Controls.Add(Panel18)
+        Panel15.Location = New Point(394, 3)
+        Panel15.Name = "Panel15"
+        Panel15.Size = New Size(404, 197)
+        Panel15.TabIndex = 5
+        ' 
+        ' Panel16
+        ' 
+        Panel16.Controls.Add(Panel17)
+        Panel16.Location = New Point(3, 197)
+        Panel16.Name = "Panel16"
+        Panel16.Size = New Size(382, 203)
+        Panel16.TabIndex = 4
+        ' 
+        ' Panel17
+        ' 
+        Panel17.Location = New Point(416, 0)
+        Panel17.Name = "Panel17"
+        Panel17.Size = New Size(420, 203)
+        Panel17.TabIndex = 3
+        ' 
+        ' Panel18
+        ' 
+        Panel18.Controls.Add(Panel19)
+        Panel18.Location = New Point(9, 200)
+        Panel18.Name = "Panel18"
+        Panel18.Size = New Size(382, 203)
+        Panel18.TabIndex = 3
+        ' 
+        ' Panel19
+        ' 
+        Panel19.Location = New Point(416, 0)
+        Panel19.Name = "Panel19"
+        Panel19.Size = New Size(420, 203)
+        Panel19.TabIndex = 3
+        ' 
+        ' Panel20
+        ' 
+        Panel20.Controls.Add(Panel21)
+        Panel20.Controls.Add(Panel23)
+        Panel20.Location = New Point(804, 3)
+        Panel20.Name = "Panel20"
+        Panel20.Size = New Size(391, 197)
+        Panel20.TabIndex = 5
+        ' 
+        ' Panel21
+        ' 
+        Panel21.Controls.Add(Panel22)
+        Panel21.Location = New Point(3, 197)
+        Panel21.Name = "Panel21"
+        Panel21.Size = New Size(382, 203)
+        Panel21.TabIndex = 4
+        ' 
+        ' Panel22
+        ' 
+        Panel22.Location = New Point(416, 0)
+        Panel22.Name = "Panel22"
+        Panel22.Size = New Size(420, 203)
+        Panel22.TabIndex = 3
+        ' 
+        ' Panel23
+        ' 
+        Panel23.Controls.Add(Panel24)
+        Panel23.Location = New Point(9, 200)
+        Panel23.Name = "Panel23"
+        Panel23.Size = New Size(382, 203)
+        Panel23.TabIndex = 3
+        ' 
+        ' Panel24
+        ' 
+        Panel24.Location = New Point(416, 0)
+        Panel24.Name = "Panel24"
+        Panel24.Size = New Size(420, 203)
+        Panel24.TabIndex = 3
+        ' 
+        ' Panel25
+        ' 
+        Panel25.Controls.Add(Panel26)
+        Panel25.Controls.Add(Panel28)
+        Panel25.Location = New Point(3, 206)
+        Panel25.Name = "Panel25"
+        Panel25.Size = New Size(385, 197)
+        Panel25.TabIndex = 5
+        ' 
+        ' Panel26
+        ' 
+        Panel26.Controls.Add(Panel27)
+        Panel26.Location = New Point(3, 197)
+        Panel26.Name = "Panel26"
+        Panel26.Size = New Size(382, 203)
+        Panel26.TabIndex = 4
+        ' 
+        ' Panel27
+        ' 
+        Panel27.Location = New Point(416, 0)
+        Panel27.Name = "Panel27"
+        Panel27.Size = New Size(420, 203)
+        Panel27.TabIndex = 3
+        ' 
+        ' Panel28
+        ' 
+        Panel28.Controls.Add(Panel29)
+        Panel28.Location = New Point(9, 200)
+        Panel28.Name = "Panel28"
+        Panel28.Size = New Size(382, 203)
+        Panel28.TabIndex = 3
+        ' 
+        ' Panel29
+        ' 
+        Panel29.Location = New Point(416, 0)
+        Panel29.Name = "Panel29"
+        Panel29.Size = New Size(420, 203)
+        Panel29.TabIndex = 3
+        ' 
+        ' Panel30
+        ' 
+        Panel30.Controls.Add(Panel31)
+        Panel30.Controls.Add(Panel33)
+        Panel30.Location = New Point(394, 206)
+        Panel30.Name = "Panel30"
+        Panel30.Size = New Size(404, 197)
+        Panel30.TabIndex = 5
+        ' 
+        ' Panel31
+        ' 
+        Panel31.Controls.Add(Panel32)
+        Panel31.Location = New Point(3, 197)
+        Panel31.Name = "Panel31"
+        Panel31.Size = New Size(382, 203)
+        Panel31.TabIndex = 4
+        ' 
+        ' Panel32
+        ' 
+        Panel32.Location = New Point(416, 0)
+        Panel32.Name = "Panel32"
+        Panel32.Size = New Size(420, 203)
+        Panel32.TabIndex = 3
+        ' 
+        ' Panel33
+        ' 
+        Panel33.Controls.Add(Panel34)
+        Panel33.Location = New Point(9, 200)
+        Panel33.Name = "Panel33"
+        Panel33.Size = New Size(382, 203)
+        Panel33.TabIndex = 3
+        ' 
+        ' Panel34
+        ' 
+        Panel34.Location = New Point(416, 0)
+        Panel34.Name = "Panel34"
+        Panel34.Size = New Size(420, 203)
+        Panel34.TabIndex = 3
+        ' 
+        ' Panel35
+        ' 
+        Panel35.Controls.Add(Panel36)
+        Panel35.Controls.Add(Panel38)
+        Panel35.Location = New Point(804, 206)
+        Panel35.Name = "Panel35"
+        Panel35.Size = New Size(391, 197)
+        Panel35.TabIndex = 6
+        ' 
+        ' Panel36
+        ' 
+        Panel36.Controls.Add(Panel37)
+        Panel36.Location = New Point(3, 197)
+        Panel36.Name = "Panel36"
+        Panel36.Size = New Size(382, 203)
+        Panel36.TabIndex = 4
+        ' 
+        ' Panel37
+        ' 
+        Panel37.Location = New Point(416, 0)
+        Panel37.Name = "Panel37"
+        Panel37.Size = New Size(420, 203)
+        Panel37.TabIndex = 3
+        ' 
+        ' Panel38
+        ' 
+        Panel38.Controls.Add(Panel39)
+        Panel38.Location = New Point(9, 200)
+        Panel38.Name = "Panel38"
+        Panel38.Size = New Size(382, 203)
+        Panel38.TabIndex = 3
+        ' 
+        ' Panel39
+        ' 
+        Panel39.Location = New Point(416, 0)
+        Panel39.Name = "Panel39"
+        Panel39.Size = New Size(420, 203)
+        Panel39.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(451, 75)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(290, 34)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1198, 552)
+        Controls.Add(FlowLayoutPanel1)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel7.ResumeLayout(False)
+        Panel3.ResumeLayout(False)
+        Panel4.ResumeLayout(False)
+        Panel10.ResumeLayout(False)
+        Panel11.ResumeLayout(False)
+        FlowLayoutPanel1.ResumeLayout(False)
+        Panel2.ResumeLayout(False)
+        Panel5.ResumeLayout(False)
+        Panel13.ResumeLayout(False)
+        Panel15.ResumeLayout(False)
+        Panel16.ResumeLayout(False)
+        Panel18.ResumeLayout(False)
+        Panel20.ResumeLayout(False)
+        Panel21.ResumeLayout(False)
+        Panel23.ResumeLayout(False)
+        Panel25.ResumeLayout(False)
+        Panel26.ResumeLayout(False)
+        Panel28.ResumeLayout(False)
+        Panel30.ResumeLayout(False)
+        Panel31.ResumeLayout(False)
+        Panel33.ResumeLayout(False)
+        Panel35.ResumeLayout(False)
+        Panel36.ResumeLayout(False)
+        Panel38.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel14 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents Panel16 As Panel
+    Friend WithEvents Panel17 As Panel
+    Friend WithEvents Panel18 As Panel
+    Friend WithEvents Panel19 As Panel
+    Friend WithEvents Panel20 As Panel
+    Friend WithEvents Panel21 As Panel
+    Friend WithEvents Panel22 As Panel
+    Friend WithEvents Panel23 As Panel
+    Friend WithEvents Panel24 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel25 As Panel
+    Friend WithEvents Panel26 As Panel
+    Friend WithEvents Panel27 As Panel
+    Friend WithEvents Panel28 As Panel
+    Friend WithEvents Panel29 As Panel
+    Friend WithEvents Panel30 As Panel
+    Friend WithEvents Panel31 As Panel
+    Friend WithEvents Panel32 As Panel
+    Friend WithEvents Panel33 As Panel
+    Friend WithEvents Panel34 As Panel
+    Friend WithEvents Panel35 As Panel
+    Friend WithEvents Panel36 As Panel
+    Friend WithEvents Panel37 As Panel
+    Friend WithEvents Panel38 As Panel
+    Friend WithEvents Panel39 As Panel
+
+End Class
---
+++ Main Menu/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Panel6_Paint(sender As Object, e As PaintEventArgs) Handles Panel6.Paint
+
+    End Sub
+
+    Private Sub Panel7_Paint(sender As Object, e As PaintEventArgs) Handles Panel7.Paint
+
+    End Sub
+
+    Private Sub Panel9_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel15_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+End Class
